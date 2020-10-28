18a5fc357a233209c64f69ac1eb959e2e4a97554
Isabel Restrepo <isabelrestre@gmail.com>
2020-10-28 14:03:06 -0400

    add more comments, update syntax

--- local-python/xnat2bids.sh
+++ local-python/xnat2bids.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# this is where you dicoms and BIDS niftii will be saved under
 OUTPUTDIR=${HOME}/bidstest
 mkdir ${OUTPUTDIR} || echo "${OUTPUTDIR} already exists"
 
+# spacify where you bidsmap file lives
 BIDSMAP=../bidsmaps/sanes_sadlum.json
 
+# replace with you XNAT user
 XNAT_USER="testuser"
+
+# replace with a XNAT assession number that you have permissions to
 XNAT_SESSION="XNAT3_E00001"
 
+# call xnat2bids, only include sequences 1 and 6
 xnat2bids ${XNAT_SESSION} ${OUTPUTDIR} -u ${XNAT_USER} -f ${BIDSMAP} -i 1 -i 6
--- oscar-sbatch/xnat2bids_single_session.sh
+++ oscar-sbatch/xnat2bids_single_session.sh
@@ -9,14 +9,14 @@
 #--------- Variables ---------
 
 # version of xnat2bids being used
-version=v0.1.5 
+version=v1.0.0 
 
 # Your working directory in Oscar, usually /gpfs/data/<your PI's group>. 
 # We pass (bind) this path to singularity so that it can access/see it                                    
 data_dir=/gpfs/data/bnc  
 
 # The first directory is where your dicoms and bids data will be exported to. 
-# The second directory is the bidsmap file for your study
+# The second path is the bidsmap file for your study
 # Both should be inside the data_dir, otherwise it won't be seen by singularity                     
 bids_root_dir=${data_dir}/shared/bids-export/${USER}
 
@@ -33,9 +33,10 @@ simg=/gpfs/data/bnc/simgs/brownbnc/xnat-tools-${version}.sif
 #--------- Run xnat2bids ---------
 
 # runs singularity command to extract DICOMs from xnat and export to BIDS
-singularity exec -B ${data_dir}                                         \
-    ${simg} xnat2bids        			                                \
-    --user ${XNAT_USER} --password "${XNAT_PASSWORD}"                   \
-    --session ${XNAT_SESSION} --session_suffix ${session_suffix}             \
-    --bids_root_dir ${bids_root_dir}                                    \
-    --bidsmap_file ${bidsmap_file}                                  	\
+# this command tells singularity to launch out xnat-tools-${version}.sif image
+# and execute yje xnat2bids command with the given inputs. The `-B ${data_dir}` makes that
+# available inside the singularity container
+singularity exec -B ${data_dir} \
+    ${simg} xnat2bids ${XNAT_SESSION} ${bids_root_dir} \     			                                \
+    -u ${XNAT_USER} -p "${XNAT_PASSWORD}" \
+    -f ${bidsmap_file}
